c6539102ecc16af362369be05268764758c409c3
peter-madigan <pmadigan@pdsf10.nersc.gov>
2016-04-27 15:30:39 -0700

    2016-04-27

--- macros/nitrogen.C
+++ macros/nitrogen.C
@@ -36,17 +36,19 @@ void nitrogen() {
   gStyle->SetOptStat(0);
   
   // Set up chains for runs
-  static const Int_t nch = 10;
-  static const Int_t nrun = 10;
+  static const Int_t nch = 20;
+  static const Int_t nrun = 5;
   TObjArray* ch = new TObjArray();
   for( Int_t ich = 0; ich < nch; ich++ )
     ch->Add(new TChain("pdsEvTree","pdsEvTree"));
   
   int ich = 0;
-  for( int i = 6166; i <= 6297; i+=(6297-6166)/nch ) {
+  //for( int i = 6166; i <= 6297; i+=(6297-6166)/nch ) {
+  for( int i = 6166; i <= 6382; i+=(6382-6166)/nch ) {
     if( ich == nch ) break;
     cout << "Chain #" << ich << " contains runs ( ";
-    for( int j = 0; j < nrun && i+j <= 6297 && ich < nch; j++ ) {
+    //for( int j = 0; j < nrun && i+j <= 6297 && ich < nch; j++ ) {
+    for( int j = 0; j < nrun && i+j <= 6382 && ich < nch; j++ ) {
       cout << i+j << " ";
       ((TChain*)ch->At(ich))->Add(Form("data/pdsTree%04d/pds*",i+j));
     }
@@ -56,11 +58,16 @@ void nitrogen() {
   
   // Set up plots
   Int_t nbins = 25;
-  Double_t xmin = 0;
-  Double_t xmax = 3.2e3;
-  TObjArray* hist = new TObjArray();
-  for( Int_t ich = 0; ich < nch; ich++ )
-    hist->Add(new TH1F(Form("hist%d",ich),";dt (ns);triplet hits",nbins,xmin,xmax));
+  Double_t tmin = 0;
+  Double_t tmax = 3.2e3;
+  Double_t rmin = 0.2;			  
+  Double_t rmax = 5;
+  TObjArray* hist_lifetime = new TObjArray();
+  TObjArray* hist_ratio    = new TObjArray();
+  for( Int_t ich = 0; ich < nch; ich++ ) {
+    hist_lifetime->Add(new TH1F(Form("hist_lifetime%d",ich),";dt (ns);triplet hits",nbins,tmin,tmax));
+    hist_ratio->Add(new TH1F(Form("hist_ratio%d",ich),";triplet/singlet;count",nbins*2,rmin,rmax));
+  }
 
   // Set up times
   Double_t time[nch];
@@ -68,12 +75,14 @@ void nitrogen() {
 
   // Set up fit
   TObjArray* fit = new TObjArray();
-  TF1* fit_func = new TF1("fit","exp([0]-x/[1])",500,3000);
+  TObjArray* fit_ratio = new TObjArray();
+  TF1* fit_func = new TF1("fit","exp([0]-x/[1])",500,2500);
   fit_func->SetParameters(2,1000);
   fit_func->SetParLimits(1,0.1,1e5);
 
   // Set branches
   for( int ich = 0; ich < nch; ich++ ) {
+    if( ((TChain*)ch->At(ich))->GetEntries() == 0 ) continue;
     ((TChain*)ch->At(ich))->SetBranchStatus("*", kFALSE);
 
     ((TChain*)ch->At(ich))->SetBranchAddress("gps_yr",&gps_yr);
@@ -119,7 +128,7 @@ void nitrogen() {
     Double_t meanTime = 0.;
     Double_t minTime = 0;
     Double_t maxTime = 0;
-    for( Int_t i = 0; ((TChain*)ch->At(ich))->GetEntry(i); i++ ) {      
+    for( Int_t i = 0; ((TChain*)ch->At(ich))->GetEntry(i); i++ ) { 
       meanTime += gps_ns * 1e-9 + gps_s + gps_d * 3600 * 24;
       if( i == 0 ) 
 	minTime = gps_ns * 1e-9 + gps_s + gps_d * 3600 * 24;
@@ -131,21 +140,32 @@ void nitrogen() {
 	     << i << " of " << ((TChain*)ch->At(ich))->GetEntriesFast() << "\n"
 	     << "Time: " << gps_ns * 1e-9 + gps_s + gps_d * 3600 * 24 << endl;
       // Cut events
-      if( inBeamWindow && pds_flag && pds_nevent == 1) {
+      if( !inBeamWindow && pds_flag && pds_nevent == 1 ) {
 	// Loop over PDS
-	Double_t TOF = pds_time[0] - rf_time - kDelay;
+	//Double_t TOF = pds_time[0] - rf_time - kDelay;
 	Double_t TOF_hit = 0;
+	Double_t triplet = 0;
+	Double_t singlet = 0;
 	for( Int_t pmt = 0; pmt < kNPMTs; pmt++ ) {
 	  if( pmt_flag[pmt] ) {
+	    if( pmt_peak[pmt][0] > 5 && 
+		Abs(pmt_time[pmt][0]-pds_time[0]) < 30 )
+	      singlet += pmt_integral[pmt][0];
 	    for( Int_t j = 0; j < pmt_hits[pmt]; j++) {
 	      TOF_hit = pmt_time[pmt][j] - pds_time[0];
 	      if( pmt_time[pmt][j] > pds_time[0] &&
-		  (pmt_peak[pmt][0] > 5 &&
-		   TOF < 103.28+77.267 && TOF > 14.54+77.267) ) // approx 100MeV - 800MeV
-		(TH1F*)hist->At(ich)->TH1F::Fill(TOF_hit);
+		  ( pmt_peak[pmt][0] > 5 ) )
+		//TOF < 103.28+77.267 && TOF > 14.54+77.267) ) // approx 100MeV - 800MeV
+		(TH1F*)hist_lifetime->At(ich)->TH1F::Fill(TOF_hit);
+	      if( pmt_time[pmt][j] > pds_time[0] &&
+		  ( pmt_peak[pmt][0] > 5 ) &&
+		  Abs(pmt_time[pmt][j]-pds_time[0]) > 30)
+		triplet += pmt_integral[pmt][j];
 	    }
 	  }
 	}
+	if( singlet != 0 || triplet != 0 )
+	  (TH1F*)hist_ratio->At(ich)->TH1F::Fill(triplet/singlet);
       }
     }
     
@@ -155,40 +175,95 @@ void nitrogen() {
     time[ich] = meanTime;
     cout << "Average run time: " << time[ich] << endl;
     
-    fit->Add((TH1F*)hist->At(ich)->TH1F::Fit("fit","sr"));
-    (TH1F*)hist->At(ich)->TH1F::Draw("e");
+    fit->Add((TH1F*)hist_lifetime->At(ich)->TH1F::Fit("fit","sr"));
+    (TH1F*)hist_lifetime->At(ich)->TH1F::Draw("e");
     vector<Double_t> N2 = GetN2AndError((TFitResultPtr)fit->At(ich));
     cout << "N2: " << N2[0] << "+/-" << N2[1] << "ppm" << endl;
+    
+    fit_ratio->Add((TH1F*)hist_ratio->At(ich)->TH1F::Fit("gaus","s"));
+    (TH1F*)hist_ratio->At(ich)->TH1F::Draw("e");
+    
     c1->SetLogy();
     c1->Update();
   }
   
-  TH1F* hN2 = new TH1F("hN2","Nitrogen concentration over WNR runs;;[N_{2}] (ppm)",
-		       nch*2-1, timeBins);
+  TH1F* hr  = new TH1F("hr","Triplet/singlet ratio;;Ratio", nch*2-1, timeBins);
+  TH1F* htT = new TH1F("htT","Triplet lifetime;;#tau_{T} (ns)", nch*2-1, timeBins);
+  TH1F* hN2 = new TH1F("hN2",";;Eq. [N_{2}] (ppm)",nch*2-1, timeBins);
+  TH1F* hO2 = new TH1F("hO2",";;Eq. [O_{2}] (ppm)",nch*2-1, timeBins);
   for( Int_t ich = 0; ich < nch; ich++ ) {
+    if(((TChain*)ch->At(ich))->GetEntries() == 0 ) continue;
     vector<Double_t> N2 = GetN2AndError((TFitResultPtr)fit->At(ich));
+    vector<Double_t> tT = GettTAndError((TFitResultPtr)fit->At(ich));
+    vector<Double_t> O2 = GetO2AndError((TFitResultPtr)fit->At(ich));
+    vector<Double_t> ratio = GetRatioAndError((TFitResultPtr)fit_ratio->At(ich));
     cout << "N2: " << N2[0] << "+/-" << N2[1] << "ppm" << endl;
     cout << "Time: " << timeBins[ich*2] << ", " << time[ich] << "," << timeBins[ich*2+1] << endl;
     hN2->Fill(time[ich], N2[0]);
     hN2->SetBinError(hN2->FindBin(time[ich]),N2[1]);
+
+    htT->Fill(time[ich], tT[0]);
+    htT->SetBinError(htT->FindBin(time[ich]),tT[1]);
+
+    hO2->Fill(time[ich], O2[0]);
+    hO2->SetBinError(hO2->FindBin(time[ich]),O2[1]);
+
+    hr->Fill(time[ich], ratio[0]);
+    hr->SetBinError(hr->FindBin(time[ich]),ratio[1]);
   }
   
   hN2->SetLineColor(kBlack);
   hN2->SetMarkerStyle(20);
   hN2->GetXaxis()->SetTimeDisplay(1);
   hN2->GetXaxis()->SetTimeFormat("%m/%d%p");
-  hN2->GetXaxis()->SetNdivisions(205);
+  //hN2->GetXaxis()->SetNdivisions(205);
   hN2->Draw("e1");
 
   c1->SetLogy(0);
   c1->SetGridx(); c1->SetGridy();
-  hN2->SaveAs("plots/nitrogen-conc.C");
+  hN2->SaveAs("plots/nitrogen-eqN2.C");
+
+  c1->DrawClone();
+  htT->SetLineColor(kBlack);
+  htT->SetMarkerStyle(20);
+  htT->GetXaxis()->SetTimeDisplay(1);
+  htT->GetXaxis()->SetTimeFormat("%m/%d%p");
+  //htT->GetXaxis()->SetNdivisions(205);                    
+  htT->Draw("e1");
+
+  c1->SetLogy(0);
+  c1->SetGridx(); c1->SetGridy();
+  htT->SaveAs("plots/nitrogen-tT.C");
+
+  c1->DrawClone();
+  hO2->SetLineColor(kBlack);
+  hO2->SetMarkerStyle(20);
+  hO2->GetXaxis()->SetTimeDisplay(1);
+  hO2->GetXaxis()->SetTimeFormat("%m/%d%p");
+  //h02->GetXaxis()->SetNdivisions(205);                                             
+  hO2->Draw("e1");
+
+  c1->SetLogy(0);
+  c1->SetGridx(); c1->SetGridy();
+  hO2->SaveAs("plots/nitrogen-eqO2.C");
+
+  c1->DrawClone();
+  hr->SetLineColor(kBlack);
+  hr->SetMarkerStyle(20);
+  hr->GetXaxis()->SetTimeDisplay(1);
+  hr->GetXaxis()->SetTimeFormat("%m/%d%p");
+  //hr->GetXaxis()->SetNdivisions(205);                                                            
+  hr->Draw("e1");
+
+  c1->SetLogy(0);
+  c1->SetGridx(); c1->SetGridy();
+  hr->SaveAs("plots/nitrogen-ratio.C");
 }
 
 using namespace TMath;
 vector<Double_t> GetN2AndError(TFitResultPtr& f) {
   vector<Double_t> N2(2,0.0);
-  if( !f || f->IsEmpty() ) return N2;
+  if( &f == NULL  || f->IsEmpty() ) return N2;
 
   Double_t tau0 = 1453;
   Double_t tau0_err = 10;
@@ -205,4 +280,49 @@ vector<Double_t> GetN2AndError(TFitResultPtr& f) {
   return N2;
 }
 
-		
+vector<Double_t> GetO2AndError(TFitResultPtr& f) {
+  vector<Double_t> O2(2,0.0);
+  if( &f == NULL  || f->IsEmpty() ) return O2;
+
+  Double_t tau0 = 1453;
+  Double_t tau0_err = 10;
+  Double_t tau1 = f->Parameter(1);
+  Double_t tau1_err = f->ParError(1);
+  Double_t kQ = 0.000540;
+  Double_t kQ_err = 0.000003;
+
+  O2[0] = (1 / tau1 - 1 / tau0 - 0.000110 * 2.2) / kQ;
+  O2[1] = Sqrt(Power(kQ_err * (1 / tau1 - 1 / tau0) / kQ / kQ, 2) +
+               Power(tau0_err / kQ / tau0 / tau0, 2) +
+               Power(tau1_err / kQ / tau1 / tau1, 2) + 
+	       Power(0.2 * 0.000110 / kQ, 2) +
+	       Power(0.000010 * 2.2 / kQ, 2) );
+
+  return O2;
+}
+
+vector<Double_t> GettTAndError(TFitResultPtr& f) {
+  vector<Double_t> tT(2,0.0);
+  if( &f == NULL || f->IsEmpty() ) return tT;
+
+  Double_t tau1 = f->Parameter(1);
+  Double_t tau1_err = f->ParError(1);
+  
+  tT[0] = tau1;
+  tT[1] = tau1_err;
+
+  return tT;
+}		
+
+vector<Double_t> GetRatioAndError(TFitResultPtr& f) {
+  vector<Double_t> r(2,0.0);
+  if( &f == NULL || f->IsEmpty() ) return r;
+
+  Double_t ratio = f->Parameter(1);
+  Double_t ratio_err = f->ParError(1);
+
+  r[0] = ratio;
+  r[1] = ratio_err;
+
+  return r;
+}
--- runSingle.sh
+++ runSingle.sh
@@ -9,32 +9,35 @@ else
 fi
 
 if [ "$datadir" == "" ]; then
-    datadir=/project/projectdirs/captain/data/2016/pmt/2016WNRRun2
+    datadirs=( /project/projectdirs/captain/data/2016/pmt/2016WNRRun2 /project/projectdirs/captain/data/2016/pmt/2016WNRRun /project/projectdirs/captain/data/2016/pmt/Cosmic )
     outdir=./data
 else
+    datadirs=( "$datadir" )
     outdir=./calib
 fi
 
 echo "Checking for run $runno..."
-if [ -d $datadir/run$runno ]; then
-    echo "Run found!"
-    i=0
-    mkdir -v $outdir/pdsTree$runno
-    for infile in `ls $datadir/run$runno`; do
-	echo "Checking $infile..."
+for datadir in "${datadirs[@]}"; do
+    if [ -d $datadir/run$runno ]; then
+	echo "Run found!"
+	i=0
+	mkdir -v $outdir/pdsTree$runno
+	for infile in `ls $datadir/run$runno`; do
+	    echo "Checking $infile..."
 	#if [ ! -f $infile ]; then
 	#    echo "...is directory. Skipping..."
 	#if [ ! -s $infile ]; then
 	#    echo "...is empty. Skipping..."
-	if [ ! ${infile: -5} == ".root" ]; then
-	    echo "...is a ${infile: -5} file. Skipping..."
-	else
-	    root -q -b "PDSAnalysis.cc+(\"$datadir/run$runno/$infile\",$runno,\"$outdir/pdsTree$runno/pdsEvTree_$i.root\",\"$opt\")"
-	    let "i++"
-	fi
-    done
-else
-    echo "ERROR: Run not found!"
-fi
+	    if [ ! ${infile: -5} == ".root" ]; then
+		echo "...is a ${infile: -5} file. Skipping..."
+	    else
+		root -q -b "PDSAnalysis.cc+(\"$datadir/run$runno/$infile\",$runno,\"$outdir/pdsTree$runno/pdsEvTree_$i.root\",\"$opt\")"
+		let "i++"
+	    fi
+	done
+    else
+	echo "ERROR: Run not found!"
+    fi
+done
 
 echo "Done!"
